958e343d2c7abbf135594d08de793b13644b9cbf
Patrick Todd <patrick.a.todd@gmail.com>
2016-08-03 18:09:56 -0400

    testing

---
+++ mean/fib_nacci/fib_nacci.js
@@ -0,0 +1,23 @@
+function fib() {
+  var num = 0;
+  var prev_num = 1;
+  var temp;
+  function nacci() {
+    temp = num;
+    num+= prev_num;
+    prev_num = temp;
+    console.log(num);
+  }
+  return nacci;
+}
+var fibCounter = fib();
+
+fibCounter();
+fibCounter();
+fibCounter();
+fibCounter();
+fibCounter();
+fibCounter();
+fibCounter();
+fibCounter();
+fibCounter();
